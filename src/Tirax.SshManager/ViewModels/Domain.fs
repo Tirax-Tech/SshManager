9895ce545010d8e6161acdfa5d0210c068d39acd
Ruxo Zheng <rz@just.sent.as>
2022-05-16 16:45:07 +0700

    Use DataGrid for display added tunnels

--- src/Tirax.SshManager/ViewModels/Domain.fs
+++ src/Tirax.SshManager/ViewModels/Domain.fs
@@ -1,24 +1,15 @@
 ﻿namespace Tirax.SshManager.ViewModels
 
-open System
-open ReactiveUI
-
 type Host = string
-type Port = int16
+type Port = uint16
 
 type Server = Host * Port
 
-type TunnelConfig() =
-    inherit ReactiveObject()
-    
-    let mutable ssh_host = String.Empty
-    let mutable ssh_port = 0us
-    let mutable local_port = 0us
-    let mutable remote_host = String.Empty
-    let mutable remote_port = 0us
-    
-    member my.SshHost with get() = ssh_host and set v = my.RaiseAndSetIfChanged(&ssh_host, v) |> ignore
-    member my.SshPort with get() = ssh_port and set v = my.RaiseAndSetIfChanged(&ssh_port, v) |> ignore
-    member my.LocalPort with get() = local_port and set v = my.RaiseAndSetIfChanged(&local_port, v) |> ignore
-    member my.RemoteHost with get() = remote_host and set v = my.RaiseAndSetIfChanged(&remote_host, v) |> ignore
-    member my.RemotePort with get() = remote_port and set v = my.RaiseAndSetIfChanged(&remote_port, v) |> ignore
+[<CLIMutable>]
+type TunnelConfig = {
+    SshHost :Host
+    SshPort :Port
+    LocalPort :Port
+    RemoteHost :Host
+    RemotePort :Port
+}
