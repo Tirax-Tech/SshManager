728d8583d46ec8f5ef5aed33b7d38efae19d6ded
Ruxo Zheng <rz@just.sent.as>
2022-05-22 06:53:55 +0700

    Ensure ssh-agent is running

--- src/Tirax.SshManager/Program.fs
+++ src/Tirax.SshManager/Program.fs
@@ -6,6 +6,17 @@ open Avalonia.ReactiveUI
 open Tirax.SshManager
 
 module Program =
+    let [<Literal>] SshAgentProcessName = "ssh-agent"
+    let ensureSSHAgentRun() =
+        let agents =
+            query {
+                for p in System.Diagnostics.Process.GetProcesses() do
+                where (p.ProcessName = SshAgentProcessName)
+                select (p.Id, p.Responding)
+            } |> Seq.toList
+        assert (agents |> Seq.forall snd)
+        if agents.Length = 0 then
+            System.Diagnostics.Process.Start(SshAgentProcessName).Dispose()
 
     [<CompiledName "BuildAvaloniaApp">] 
     let buildAvaloniaApp () = 
@@ -17,4 +28,5 @@ module Program =
 
     [<EntryPoint; STAThread>]
     let main argv =
+        ensureSSHAgentRun()
         buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
