d948d1ca2be2751b928e710829d8f80a9abc4bf9
Ruxo Zheng <rz@just.sent.as>
2022-05-22 08:24:36 +0700

    Add Migration Steps to Isolated Storage instead of a local file

--- src/Tirax.SshManager/RzActor.fs
+++ src/Tirax.SshManager/RzActor.fs
@@ -13,19 +13,22 @@ and FsActorReceivable =
     abstract member FsReceive<'T> :(struct (ActorContext * 'T) -> unit) -> unit
     abstract member FsReceiveAsync<'T> :(struct (ActorContext * 'T) -> Async<unit>) -> unit
 
+type FsFuncReceiver<'T> = (struct (ActorContext * 'T)) -> unit
+type FsFuncAsyncReceiver<'T> = (struct (ActorContext * 'T)) -> Async<unit>
+
 type FsReceiveActor() =
     inherit ReceiveActor()
     
-    member my.FsReceive<'T>(f :struct (ActorContext * 'T) -> unit) =
+    member my.FsReceive<'T>(f :FsFuncReceiver<'T>) =
         // TODO: check if Sender is really correct!
         base.Receive<'T>(fun v -> f (my.GetContext(), v))
             
-    member my.FsReceiveAsync<'T>(f :struct (ActorContext * 'T) -> Async<unit>) =
+    member my.FsReceiveAsync<'T>(f :FsFuncAsyncReceiver<'T>) =
         base.ReceiveAsync(fun v -> f (my.GetContext(), v) |> Async.StartImmediateAsTask :> Task)
         
     interface FsActorReceivable with
-        member my.FsReceive<'T>(f :struct (ActorContext * 'T) -> unit) = my.FsReceive<'T>(f)
-        member my.FsReceiveAsync<'T>(f :struct (ActorContext * 'T) -> Async<unit>) = my.FsReceiveAsync<'T>(f)
+        member my.FsReceive<'T>(f :FsFuncReceiver<'T>) = my.FsReceive<'T>(f)
+        member my.FsReceiveAsync<'T>(f :FsFuncAsyncReceiver<'T>) = my.FsReceiveAsync<'T>(f)
             
     member my.Become (action :Action) = base.Become action
         
--- src/Tirax.SshManager/Storage.fs
+++ src/Tirax.SshManager/Storage.fs
@@ -2,8 +2,10 @@
 module Tirax.SshManager.Storage
 
 open System.IO
+open System.IO.IsolatedStorage
 open System.Text.Json
 open Akka.Actor
+open RZ.FSharp.Akka
 open RZ.FSharp.Extension
 open Tirax.SshManager.ViewModels
 
@@ -24,22 +26,40 @@ type Storage(actor :IActorRef) =
 let deserializeTunnelConfig (s :string) = JsonSerializer.Deserialize<TunnelConfig seq> s
 
 let loadFile (file :FileInfo) =
-    if file.Exists then
-        let content = File.ReadAllText file.FullName
-        Option.safeCall deserializeTunnelConfig content
-    else
-        None
+    if file.Exists
+    then Some (File.ReadAllText file.FullName)
+    else None
         
 type FileManager(option) as my =
-    inherit ReceiveActor()
+    inherit FsReceiveActor()
     
-    do my.Receive<Load>(my.Load)
-    do my.Receive<Save>(my.Save)
+    let iso_store = IsolatedStorageFile.GetUserStoreForApplication()
     
-    member private my.Load _ :unit =
-        let data = option.DataFile |> loadFile |> Option.defaultValue Seq.empty
-        in my.Sender.Tell (LoadResult data)
+    let openDataFile mode = iso_store.OpenFile("ssh-manager.json", mode)
+    
+    let load struct (my, Load) =
+        use data_file = openDataFile FileMode.OpenOrCreate
+        let iso_data = if data_file.Length = 0
+                       then None
+                       else Some <| StreamReader(data_file).ReadToEnd()
+        data_file.Close()
+        let data = iso_data |> Option.orElseWith (fun _ -> option.DataFile |> loadFile)
+                            |> Option.bind (Option.safeCall deserializeTunnelConfig)
+                            |> Option.defaultValue Seq.empty
+                            |> Seq.toArray
+        let need_migration = data_file.Length = 0 && data.Length > 0
+        if need_migration then my.Self.Tell (Save data)
+        my.Sender.Tell (LoadResult data)
         
-    member private my.Save (Save tunnels) :unit =
+    let save struct (_, Save tunnels) =
         let content = JsonSerializer.Serialize<TunnelConfig seq> tunnels
-        in File.WriteAllText(option.DataFile.FullName, content)
+        use data_file = openDataFile FileMode.Create |> StreamWriter
+        data_file.Write(content)
+        data_file.Close()
+        if option.DataFile.Exists then option.DataFile.Delete()
+    
+    do my.FsReceive(load)
+    do my.FsReceive(save)
+    
+    override _.PostStop () =
+        iso_store.Dispose()
