a9a0a0ee3d84d056f713076a624c715a9a234e67
Ruxo Zheng <rz@just.sent.as>
2022-05-22 08:24:36 +0700

    Add Migration Steps to Isolated Storage instead of a local file

--- src/Tirax.SshManager/Storage.fs
+++ src/Tirax.SshManager/Storage.fs
@@ -2,8 +2,10 @@
 module Tirax.SshManager.Storage
 
 open System.IO
+open System.IO.IsolatedStorage
 open System.Text.Json
 open Akka.Actor
+open RZ.FSharp.Akka
 open RZ.FSharp.Extension
 open Tirax.SshManager.ViewModels
 
@@ -25,21 +27,38 @@ let deserializeTunnelConfig (s :string) = JsonSerializer.Deserialize<TunnelConfi
 
 let loadFile (file :FileInfo) =
     if file.Exists then
-        let content = File.ReadAllText file.FullName
-        Option.safeCall deserializeTunnelConfig content
+        Some (File.ReadAllText file.FullName)
     else
         None
         
 type FileManager(option) as my =
-    inherit ReceiveActor()
+    inherit FsReceiveActor()
     
-    do my.Receive<Load>(my.Load)
-    do my.Receive<Save>(my.Save)
+    let iso_store = IsolatedStorageFile.GetUserStoreForApplication()
     
-    member private my.Load _ :unit =
-        let data = option.DataFile |> loadFile |> Option.defaultValue Seq.empty
-        in my.Sender.Tell (LoadResult data)
+    let openDataFile () = iso_store.OpenFile("ssh-manager.json", FileMode.OpenOrCreate)
+    
+    let load struct (my, Load) =
+        use data_file = openDataFile()
+        let iso_data = if data_file.Length = 0
+                       then None
+                       else Some <| StreamReader(data_file).ReadToEnd()
+        let data = iso_data |> Option.orElseWith (fun _ -> option.DataFile |> loadFile)
+                            |> Option.bind (Option.safeCall deserializeTunnelConfig)
+                            |> Option.defaultValue Seq.empty
+                            |> Seq.toArray
+        let need_migration = data_file.Length = 0 && data.Length > 0
+        if need_migration then my.Self.Tell (Save data)
+        my.Sender.Tell (LoadResult data)
         
-    member private my.Save (Save tunnels) :unit =
+    let save struct (_, Save tunnels) =
         let content = JsonSerializer.Serialize<TunnelConfig seq> tunnels
-        in File.WriteAllText(option.DataFile.FullName, content)
+        use data_file = openDataFile() |> StreamWriter
+        data_file.Write(content)
+        if option.DataFile.Exists then option.DataFile.Delete()
+    
+    do my.FsReceive(load)
+    do my.FsReceive(save)
+    
+    override _.PostStop () =
+        iso_store.Dispose()
