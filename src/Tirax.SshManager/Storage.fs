260e74a5cce3a5b819da4dfcfb2c7b91b0e72959
Ruxo Zheng <rz@just.sent.as>
2022-05-29 15:22:10 +0700

    Fix TunnelConfig's running statuses were saved into storage too.

--- src/Tirax.SshManager/Storage.fs
+++ src/Tirax.SshManager/Storage.fs
@@ -19,12 +19,36 @@ type Storage(actor :IActorRef) =
     member _.Load() = async { return! actor.Ask<LoadResult>(Load) |> Async.AwaitTask }
     member _.Save tunnels = actor.Tell (Save tunnels)
 
-let deserializeTunnelConfig (s :string) = JsonSerializer.Deserialize<TunnelConfig seq> s
-
 let loadFile (file :FileInfo) =
     if file.Exists
     then Some (File.ReadAllText file.FullName)
     else None
+    
+type TunnelConfigStorageModel = {
+    Name :string
+    SshHost :string
+    SshPort :uint16
+    LocalPort :uint16
+    RemoteHost :string
+    RemotePort :uint16
+}
+with
+    static member FromTunnelConfig (c :TunnelConfig) =
+        { Name = c.Name
+          SshHost = c.SshHost
+          SshPort = c.SshPort
+          LocalPort = c.LocalPort
+          RemoteHost = c.RemoteHost
+          RemotePort = c.RemotePort }
+    static member ToTunnelConfig s =
+        TunnelConfig(Name = s.Name,
+                     SshHost = s.SshHost,
+                     SshPort = s.SshPort,
+                     LocalPort = s.LocalPort,
+                     RemoteHost = s.RemoteHost,
+                     RemotePort = s.RemotePort)
+
+let deserializeTunnelConfig (s :string) = JsonSerializer.Deserialize<TunnelConfigStorageModel seq> s
         
 type FileManager() as my =
     inherit FsReceiveActor()
@@ -41,12 +65,14 @@ type FileManager() as my =
         data_file.Close()
         let data = iso_data |> Option.bind (Option.safeCall deserializeTunnelConfig)
                             |> Option.defaultValue Seq.empty
+                            |> Seq.map TunnelConfigStorageModel.ToTunnelConfig
                             |> Seq.toArray
         my.Sender.Tell (LoadResult data)
         
     let save struct (_, Save tunnels) =
         use data_file = openDataFile FileMode.Create |> StreamWriter
-        tunnels |> JsonSerializer.Serialize<TunnelConfig seq>
+        tunnels |> Seq.map TunnelConfigStorageModel.FromTunnelConfig
+                |> JsonSerializer.Serialize
                 |> data_file.Write
         data_file.Close()
     
