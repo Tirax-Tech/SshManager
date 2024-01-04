4293841f793a02ab366dac6827714ef127ab55ee
Ruxo Zheng <rz@just.sent.as>
2024-01-04 08:00:32 +0700

    Split main window into views

---
+++ src/SshManager/Views/ListConnections.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tirax.SshManager.Views;
+
+public partial class ListConnections : UserControl
+{
+    public ListConnections() {
+        InitializeComponent();
+    }
+}
