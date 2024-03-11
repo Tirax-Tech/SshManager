4a4bfc4dad5d3680023918ae01bee280aa70faaf
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-03-12 06:34:18 +0700

    Upgrade to .NET 8

---
+++ src/SshManager/Components/AppBar.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tirax.SshManager.Components;
+
+public partial class AppBar : UserControl
+{
+    public static readonly StyledProperty<string> MainIconProperty = AvaloniaProperty.Register<AppBar, string>(nameof(MainIcon), defaultValue: "Menu");
+    public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<AppBar, string>(nameof(Title), defaultValue: "Title");
+    
+    public AppBar() {
+        InitializeComponent();
+    }
+
+    public string MainIcon {
+        get => GetValue(MainIconProperty);
+        set => SetValue(MainIconProperty, value);
+    }
+
+    public string Title {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+}
